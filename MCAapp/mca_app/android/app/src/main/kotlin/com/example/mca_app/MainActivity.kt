690f7fefd775edc44c9dbfddf4bd4c83cfdb1537
kshitija <kshitijasaitavdekar.work03@gmail.com>
2025-05-13 06:05:07 +0530

    Added lib and assets folders for all Flutter projects

--- MCAapp/mca_app/android/app/src/main/kotlin/com/example/mca_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mca_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- newchildapp/child_app/android/app/src/main/kotlin/com/example/child_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.child_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
